43bb808d2c271fa364d7dd02e81ae93d7c4bc6ef
JAOC1930 <jhoelordonez2@gmail.com>
2025-08-14 16:05:12 -0500

    5

--- Portafolio_Jhoel/settings.py
+++ Portafolio_Jhoel/settings.py
@@ -81,20 +81,11 @@ WSGI_APPLICATION = 'Portafolio_Jhoel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if os.environ.get('RENDER'):  # Si estamos en Render
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600
-        )
-    }
-else:  # En desarrollo local usa SQLite
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 
 
 # Password validation
