687c7d89263ba6d9da0df95ad2b96e06de9345e9
JAOC1930 <jhoelordonez2@gmail.com>
2025-08-14 17:04:35 -0500

    gfg

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
-#!/usr/bin/env bash
+#*/usr/bin/env bash
+
 set -o errexit
 
-# Instalar dependencias
 pip install -r requirements.txt
 
-# Aplicar migraciones
-python manage.py migrate
-
-# Recolectar archivos estáticos
 python manage.py collectstatic --noinput
+python manage.py migrate
